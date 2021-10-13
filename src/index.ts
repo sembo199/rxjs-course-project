606533d6c8a7a14ab2972983f216464bde227409
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-13 14:37:17 +0200

    Section 1: Getting Started

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import {
 
 // name$.subscribe(val => console.log(val));
 
-storeDataOnServer('Some value').subscribe(
-  value => console.log(value)
-);
+// storeDataOnServer('Some value').subscribe(
+//   value => console.log(value)
+// );
+
+storeDataOnServerError('Some value').subscribe({
+  next: value => console.log(value),
+  error: error => console.log('Error whens saving:', error.message)
+});
