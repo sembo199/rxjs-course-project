6356ecd9d484cc3f641c27c5b58573ee5dd1c346
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-20 15:24:33 +0200

    25-teardown-complete-notification

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,20 @@ const observable$ = new Observable<string>(subscriber => {
   console.log("Observable executed");
   subscriber.next('Alice');
   subscriber.next('Ben');
-  setTimeout(() => subscriber.next('Charlie'), 2000);
+  setTimeout(() => {
+    subscriber.next('Charlie')
+    subscriber.complete();
+  }, 2000);
+
+  return () => {
+    console.log('Teardown');
+  };
 });
 console.log("Before subscribe");
-observable$.subscribe(value => console.log(value));
+observable$.subscribe({ 
+  next: value => console.log(value),
+  complete: () => console.log("Completed")
+});
 console.log("After subscribe");
 
 
