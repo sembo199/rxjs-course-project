3bf3fa7f5833ff51b79d8d8e147cbbb8e2802173
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-20 15:18:24 +0200

    23-next-notification

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 
 import { Observable } from "rxjs";
 
-const observable$ = new Observable(subscriber => {
+const observable$ = new Observable<string>(subscriber => {
   console.log("Observable executed");
+  subscriber.next('Alice');
 });
 console.log("Before subscribe");
-observable$.subscribe();
+observable$.subscribe(value => console.log(value));
 console.log("After subscribe");
 
 
