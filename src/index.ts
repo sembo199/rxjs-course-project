91436dcb4d27970c4a8c4a3236ce3056e66dc263
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-20 15:20:31 +0200

    24-asynchronous-emission

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { Observable } from "rxjs";
 const observable$ = new Observable<string>(subscriber => {
   console.log("Observable executed");
   subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(() => subscriber.next('Charlie'), 2000);
 });
 console.log("Before subscribe");
 observable$.subscribe(value => console.log(value));
