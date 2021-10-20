8459610ace4be47d4117e6e69845f0148b3650cd
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-20 16:09:55 +0200

    32-cold-observable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 /** SECTION 4 */
 
+import {ajax} from 'rxjs/ajax';
 
+const ajax$ = ajax<any>('https://random-data-api.com/api/name/random_name');
+
+ajax$.subscribe(data => console.log("Sub 1: ", data.response.first_name));
+ajax$.subscribe(data => console.log("Sub 2: ", data.response.first_name));
+ajax$.subscribe(data => console.log("Sub 3: ", data.response.first_name));
 
 /** SECTION 3 */
 
