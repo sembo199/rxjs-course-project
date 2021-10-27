9a76f0dfa09fb72d8563dcfa93879ffbf37e956a
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-10-27 14:52:07 +0200

    37-of-creation-function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 /** SECTION 5 */
 
+import { Observable, of } from "rxjs";
+
+// of('Alice', 'Ben', 'Charlie').subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log("Completed")
+// });
+
+ourOwnOf('Alice', 'Ben', 'Charlie').subscribe({
+  next: value => console.log(value),
+  complete: () => console.log("Completed Own Of")
+});
+
+// const names$ = new Observable<string>(subscriber => {
+//   subscriber.next('Alice');
+//   subscriber.next('Ben');
+//   subscriber.next('Charlie');
+//   subscriber.complete();
+// });
+
+// names$.subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log("Completed")
+// });
+
+function ourOwnOf(...args: string[]): Observable<string> {
+  return new Observable<string>(subscriber => {
+    for(let i = 0; i < args.length; i++) {
+      subscriber.next(args[i]);
+    }
+    subscriber.complete();
+  });
+}
+
 /** SECTION 4 */
 
 // import { Observable } from 'rxjs';
